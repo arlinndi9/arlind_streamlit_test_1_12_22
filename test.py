cbeaae26b37e6a6f56f658115861435c7878fa69
arlinndi9 <arlinndi9@gmail.com>
2022-12-25 16:35:57 +0100

    u5

--- test.py
+++ test.py
@@ -5,12 +5,18 @@ def convert_list(utext):
     return lista
 
 def convert_lower(text):
-
     lowercaselist=[]
     for i in text:
         i=i.lower()
         lowercaselist.append(i)
     return lowercaselist
+
+def convert_upper(text):
+    uppercaseletter=[]
+    for i in text:
+        i=i.upper()
+        uppercaseletter.append(i)
+    return uppercaseletter
 
 def count(text_list):
     return len(text_list)
@@ -22,23 +28,9 @@ if st.button('lista'):
     st.write(convert_list(text))
 if st.button('lower'):
     st.write(convert_lower(clist))
+if st.button('upper'):
+    st.write(convert_upper(clist))
 if st.button('count'):
     st.write('count ',count(clist))
-'''
-chose = st.radio(
-    "chose",
-    ('lista', 'lower', 'count'))
 
-if chose=='lista':
-    st.write(convert_list(text))
-if chose=='lower':
-    st.write(convert_upper(clist))
-if chose=='count':
-    st.write('count ',count(clist))
-
-lista = st.checkbox('lista')
-lowerl = st.checkbox('listalower')
-if lista and lowerl:
-    st.write(convert_list(text),convert_upper(clist))
-'''
 
